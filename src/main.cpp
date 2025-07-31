2a2ff4ab86f4b7269aa53dbb9f9521de0ff175b3
Moskito <papapleygames@gmail.com>
2025-07-31 16:06:41 +0530

    Added 1. compile command 2. wrote draw circle code. 3. made clangd setup

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "My SFML Game");
 
+    // Create a circle
+    sf::CircleShape circle(50.f); // radius = 50 pixels
+    circle.setFillColor(sf::Color::Green); // set fill color
+    circle.setPosition(375.f, 275.f); // center it roughly on the screen
+
+
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -11,7 +18,10 @@ int main() {
         }
 
         window.clear();
+
         // draw your stuff
+        window.draw(circle);
+
         window.display();
     }
 
